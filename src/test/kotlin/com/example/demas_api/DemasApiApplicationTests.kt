9ed5a1ffb69cc5ff5f8fc71b3bb8f8fbfb1bf2f1
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-16 20:10:33 -0300

    initial commit

---
+++ src/test/kotlin/com/example/demas_api/DemasApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demas_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemasApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
