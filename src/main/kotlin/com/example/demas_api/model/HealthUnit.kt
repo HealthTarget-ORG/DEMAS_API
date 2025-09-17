bb496e05787cedaeb49f382a83bc44788fe60041
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-17 02:10:16 -0300

    feat: Add HealthUnit model

---
+++ src/main/kotlin/com/example/demas_api/model/HealthUnit.kt
@@ -0,0 +1,39 @@
+package com.example.demas_api.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
+import java.time.LocalDate
+
+@Document(collection = "health_units")
+data class HealthUnit(
+    @Id val cnesCode: String,
+
+    @Field("name")
+    val name: String,
+
+    @Field("address")
+    val address: Address,
+
+    @Field("location")
+    val location: GeoJsonPoint,
+
+    @Field("city")
+    val city: String,
+
+    @Field("state")
+    val state: String,
+
+    @Field("phone")
+    val phone: String?,
+
+    @Field("email")
+    val email: String?,
+
+    @Field("last_stock_update")
+    val lastStockUpdate: LocalDate,
+
+    @Field("medicines")
+    val medicines: List<MedicineStock> = listOf()
+)
