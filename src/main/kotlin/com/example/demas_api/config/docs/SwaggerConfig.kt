a497356e486ea8bbfd2e91835ca4e67ea01d1ef4
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-17 10:11:21 -0300

    doc: Update Swagger documentation

--- src/main/kotlin/com/example/demas_api/config/docs/SwaggerConfig.kt
+++ src/main/kotlin/com/example/demas_api/config/docs/SwaggerConfig.kt
@@ -16,20 +16,31 @@ class SwaggerConfig {
                 Info()
                     .title("API de Dados de Saúde - Consulta de Medicamentos")
                     .version("1.0.0")
-                    .description(
-                        "O propósito desta API é fornecer dados consolidados e de fácil acesso sobre a " +
-                                "disponibilidade de medicamentos em unidades de saúde públicas. Ela atua como uma " +
-                                "camada intermediária, consumindo, processando e agregando dados públicos para " +
-                                "otimizar a consulta e promover a transparência.\n\n" +
-                                "A principal fonte de dados é a **DEMAS - API de Dados Abertos do Ministério da Saúde** " +
-                                "(https://apidadosabertos.saude.gov.br). Esta API resolve as complexidades da fonte " +
-                                "original, como a necessidade de paginação extensiva e a falta de agrupamentos, " +
-                                "oferecendo endpoints rápidos e prontos para o consumo por aplicações cliente."
-                    )
+                    .description("""
+                        ### Propósito Principal
+                        O objetivo desta API é fornecer dados consolidados e de fácil acesso sobre a disponibilidade de medicamentos em unidades de saúde públicas do município de Boa Viagem - CE. Ela atua como uma camada intermediária que consome, processa e agrega dados públicos para otimizar a consulta e promover a transparência na saúde.
+
+                        ### Contexto do Projeto
+                        Este projeto foi desenvolvido como parte da disciplina de Projeto Integrador do curso de **Análise e Desenvolvimento de Sistemas do IFCE _campus_ Boa Viagem**.
+
+                        ### Fonte de Dados e Avisos
+                        - A principal fonte de dados é a **DEMAS - API de Dados Abertos do Ministério da Saúde** ([apidadosabertos.saude.gov.br](https://apidadosabertos.saude.gov.br)).
+                        - Nossa API resolve as complexidades da fonte original (como a necessidade de paginação extensiva e a falta de agrupamentos), oferecendo endpoints rápidos e prontos para consumo.
+                        - A atualização dos dados na fonte original pode levar, em média, até 17 dias. Nosso sistema é projetado para buscar e processar a versão mais recente dos dados disponíveis diariamente.
+                        - Quaisquer discrepâncias nas informações, relativamente aos dados fornecidos pelo DEMAS, devem ser reportadas à equipe de desenvolvimento do projeto do governo federal.
+
+                        ---
+
+                        ### Equipe e Colaboradores
+                        - **Douglas Holanda** - Desenvolvedor - [GitHub](https://github.com/Doug16Yanc/)
+                        - **Kaiane Maciel** - Desenvolvedora - [GitHub](https://github.com/KaianeSousa/)
+                        - **Letícia do Vale** - Desenvolvedora - [GitHub](https://github.com/Leititcia)
+                        - **Wesley Rodrigues** - Desenvolvedor - [GitHub](https://github.com/Wesley00s/)
+                    """.trimIndent())
                     .contact(
                         Contact()
+                            .name("Contato Principal do Projeto")
                             .email("wesley300rodrigues@gmail.com")
-                            .name("Wesley Rodrigues")
                             .url("https://github.com/Wesley00s/")
                     )
                     .license(
