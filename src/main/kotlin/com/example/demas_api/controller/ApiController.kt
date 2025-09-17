cc1744fea5e503a75e2a4e0ea2a82a7c9a6816f0
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-17 02:37:10 -0300

    feat: Add Swagger tag to Health Controller

--- src/main/kotlin/com/example/demas_api/controller/ApiController.kt
+++ src/main/kotlin/com/example/demas_api/controller/ApiController.kt
@@ -1,6 +1,7 @@
 package com.example.demas_api.controller
 
 import com.example.demas_api.dto.MessageResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/health")
+@Tag(name = "Health Controller", description = "Endpoint para Health Check da API")
 class ApiController {
     @GetMapping
     fun health() = ResponseEntity.status(HttpStatus.OK).body(MessageResponse("Healthy"))
