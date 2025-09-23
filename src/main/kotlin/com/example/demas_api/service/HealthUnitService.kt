3c74d49482a9d67cc420efe85223c125c1c24932
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-23 14:36:10 -0300

    update: Update aggreged medicines to use medicine classification

--- src/main/kotlin/com/example/demas_api/service/HealthUnitService.kt
+++ src/main/kotlin/com/example/demas_api/service/HealthUnitService.kt
@@ -3,6 +3,7 @@ package com.example.demas_api.service
 import com.example.demas_api.dto.MedicineTotalStock
 import com.example.demas_api.model.HealthUnit
 import com.example.demas_api.model.MedicineStock
+import com.example.demas_api.model.enumeration.DrugClassification
 import com.example.demas_api.model.enumeration.LocationType
 import com.example.demas_api.model.enumeration.MedicineAvailability
 import com.example.demas_api.repository.HealthUnitRepository
@@ -49,26 +50,35 @@ class HealthUnitService(
         page: Int,
         size: Int,
         searchTerm: String,
-        filter: MedicineAvailability
+        filter: MedicineAvailability,
+        classification: DrugClassification
     ): Page<MedicineTotalStock> {
         val pageable = PageRequest.of(page, size)
-        val search = if (searchTerm.isBlank()) ".*" else Pattern.quote(searchTerm)
+        val search = searchTerm.ifBlank { ".*" }
+
+        val classificationRegex = when (classification) {
+            DrugClassification.EXPENSIVE -> "1\\.A|1\\.B"
+            DrugClassification.BASIC -> "^((?!1\\.A|1\\.B).)*\$"
+            DrugClassification.ALL -> ".*"
+        }
 
         val content: List<MedicineTotalStock>
         val totalElements: Long
 
         when (filter) {
             MedicineAvailability.AVAILABLE -> {
-                content = healthUnitRepository.aggregateStockAvailable(search, pageable)
-                totalElements = healthUnitRepository.countStockAvailable(search)
+                content = healthUnitRepository.aggregateStockAvailable(search, classificationRegex, pageable)
+                totalElements = healthUnitRepository.countStockAvailable(search, classificationRegex)
             }
+
             MedicineAvailability.UNAVAILABLE -> {
-                content = healthUnitRepository.aggregateStockUnavailable(search, pageable)
-                totalElements = healthUnitRepository.countStockUnavailable(search)
+                content = healthUnitRepository.aggregateStockUnavailable(search, classificationRegex, pageable)
+                totalElements = healthUnitRepository.countStockUnavailable(search, classificationRegex)
             }
+
             MedicineAvailability.ALL -> {
-                content = healthUnitRepository.aggregateStockAll(search, pageable)
-                totalElements = healthUnitRepository.countStockAll(search)
+                content = healthUnitRepository.aggregateStockAll(search, classificationRegex, pageable)
+                totalElements = healthUnitRepository.countStockAll(search, classificationRegex)
             }
         }
 
