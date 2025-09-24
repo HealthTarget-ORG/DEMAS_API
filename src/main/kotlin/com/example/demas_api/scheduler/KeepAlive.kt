98271fe2b0790e122c0b0bc223df6f554af6804e
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-24 18:16:28 -0300

    refactor: add health check url in environment variables

--- src/main/kotlin/com/example/demas_api/scheduler/KeepAlive.kt
+++ src/main/kotlin/com/example/demas_api/scheduler/KeepAlive.kt
@@ -1,22 +1,26 @@
 package com.example.demas_api.scheduler
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
 @Component
 class KeepAlive (
-    webClientBuilder: WebClient.Builder
+    webClientBuilder: WebClient.Builder,
+
+    @param:Value("\${health-check.url}")
+    private val healthCheckUrl: String
 ) {
     private val logger = LoggerFactory.getLogger(KeepAlive::class.java)
 
     private val webClient = webClientBuilder.build()
 
-    @Scheduled(fixedRate = 100000, initialDelay = 30000)
+    @Scheduled(fixedRate = 100000, initialDelay = 100000)
     fun keepAlive() {
         val response = webClient.get()
-            .uri("https://demas-api.onrender.com/health")
+            .uri(healthCheckUrl)
             .retrieve()
             .toBodilessEntity()
             .block()
